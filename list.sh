38ad87f8364ebf7e9fe4500608f99e17a0efde4f
Ryan Krawczyk <39915012+ryankrawz@users.noreply.github.com>
2025-12-08 10:28:24 -0500

    Nicer formatting for list.sh

--- list.sh
+++ list.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-ls -l ./.venv | awk '{print $9, "  ", $6, $7, $8}'
+VENV_DIR="${1:-.venv}"
+
+ls -l "$VENV_DIR" | awk '
+BEGIN {
+    # Header
+    printf "%-30s %-20s\n", "ENV NAME", "LAST MODIFIED";
+    print  "------------------------------ --------------------";
+}
+NR > 1 {
+    # Skip the "total" line and format the rest
+    printf "%-30s %-20s\n", $9, $6 " " $7 " " $8;
+}'
